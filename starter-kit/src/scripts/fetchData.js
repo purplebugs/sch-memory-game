e94dec7657a0a2866e2c5873934234961f8337d8
Anita Lipsky <anita@purplebugs.com>
2024-10-09 10:10:27 +0200

    refactor: init and data fetching

--- starter-kit/src/scripts/fetchData.js
+++ starter-kit/src/scripts/fetchData.js
@@ -1,15 +1,5 @@
-import { default as Game } from './game';
-
-const fetchData = async () => {
+export const fetchData = async () => {
   const res = await fetch('http://localhost:3002/api/cards');
   const data = await res.json();
   return data;
 };
-
-const init = async () => {
-  const data = await fetchData();
-  const game = new Game(data);
-  console.log('game', game);
-};
-
-init();
--- starter-kit/src/scripts/index.js
+++ starter-kit/src/scripts/index.js
@@ -1,4 +1,12 @@
 import '../styles/index.scss';
-import './fetchData.js';
-import './card.js';
-import './game.js';
+import { fetchData } from './fetchData.js';
+
+import { default as Game } from './game';
+
+const init = async () => {
+  const data = await fetchData();
+  const game = new Game(data);
+  console.log('game', game);
+};
+
+init();
